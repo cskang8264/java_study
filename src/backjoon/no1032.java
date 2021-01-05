c65b8f1921af64472176f352e0ed42f5a37e3fd8
gangdaewon <cskang8264@naver.com>
2021-01-05 20:35:10 +0900

    no1032 ing...

---
+++ src/backjoon/no1032.java
@@ -0,0 +1,13 @@
+package backjoon;
+
+import java.io.*;
+
+public class no1032 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+    }
+}
