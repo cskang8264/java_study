60e45baf94409c4c33751866c55672aa0e2f883d
gangdaewon <cskang8264@naver.com>
2021-04-11 15:46:19 +0900

    code : kakao 2020 문자열 압축

---
+++ src/kakao2020/StringZip.java
@@ -0,0 +1,39 @@
+package kakao2020;
+
+public class StringZip {
+    public static void main(String[] args) {
+        StringZip test = new StringZip();
+        test.solution("aabbaccc");
+    }
+
+    public int solution(String s) {
+        int answer = s.length();
+        for (int i = 1; i < s.length()/2; i++) {
+            int pos = 0;
+            int len = s.length();
+            for (; pos + i <= s.length(); ) {
+                String unit = s.substring(pos,pos+i);
+                pos += i;
+                int cnt = 0;
+                for (; pos + i <= s.length(); ) {
+                    if (unit.equals(s.substring(pos,pos+i))) {
+                        ++cnt;
+                        pos += i;
+                    } else {
+                        break;
+                    }
+                }
+                if (cnt > 0 ){
+                    len -= i*cnt;
+                    if (cnt < 9 ) len += 1;
+                    else if (cnt < 99) len+=2;
+                    else if (cnt < 999) len+=3;
+                    else len+=4;
+                }
+            }
+            answer = Math.min(answer,len);
+        }
+        System.out.println("answer = " + answer);
+        return answer;
+    }
+}
