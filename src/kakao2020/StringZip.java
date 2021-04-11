b7e8b7509f32347078b7f6f7d534bce2410c8564
gangdaewon <cskang8264@naver.com>
2021-04-11 15:50:28 +0900

    hihi

--- src/kakao2020/StringZip.java
+++ src/kakao2020/StringZip.java
@@ -11,12 +11,17 @@ public class StringZip {
         for (int i = 1; i < s.length()/2; i++) {
             int pos = 0;
             int len = s.length();
+            System.out.println("loof stage = " + i);
+            System.out.println("answer = " + answer);
+            System.out.println("s = " + s);
             for (; pos + i <= s.length(); ) {
                 String unit = s.substring(pos,pos+i);
                 pos += i;
                 int cnt = 0;
                 for (; pos + i <= s.length(); ) {
+                    System.out.println("cnt = " + cnt);
                     if (unit.equals(s.substring(pos,pos+i))) {
+                        System.out.println("equals unit = " + s.charAt(pos));
                         ++cnt;
                         pos += i;
                     } else {
@@ -24,6 +29,7 @@ public class StringZip {
                     }
                 }
                 if (cnt > 0 ){
+                    System.out.println("len = " + len);
                     len -= i*cnt;
                     if (cnt < 9 ) len += 1;
                     else if (cnt < 99) len+=2;
